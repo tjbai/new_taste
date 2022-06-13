236db84bd69cb40cff069394d0169bc087380068
TJ Bai <tjianbai@gmail.com>
2022-06-13 11:18:56 -0400

    hid my secrets

--- music-data/secret.py
+++ music-data/secret.py
@@ -1,7 +1,7 @@
 import tekore as tk 
 
-CLIENT_ID = '52793ece707444a59a7c4c52dbac71ad'
-CLIENT_SECRET = '15e1a48beb114b9aa112b1cf74b32690'
+CLIENT_ID = 'nice try'
+CLIENT_SECRET = 'made you look'
 
 def authorize():
     app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
